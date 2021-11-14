65d26049fff02d25c14648ddc6610cdca6baa66c
Emad Aldomari <ea@ea.sa>
2021-11-14 17:04:58 +0300

    use of sweetalert react component

--- frontend/src/context.js
+++ frontend/src/context.js
@@ -2,19 +2,18 @@ import React from "react";
 import Swal from 'sweetalert2'
 import withReactContent from 'sweetalert2-react-content'
 export const ContextStore = React.createContext({});
-
 const MySwal = withReactContent(Swal)
 
 export default function Context({children}) {
-    const Toast = Swal.mixin({
+    const Toast = MySwal.mixin({
         toast: true,
         position: 'top-end',
         showConfirmButton: false,
         timer: 3000,
         timerProgressBar: true,
         didOpen: (toast) => {
-          toast.addEventListener('mouseenter', Swal.stopTimer)
-          toast.addEventListener('mouseleave', Swal.resumeTimer)
+          toast.addEventListener('mouseenter', MySwal.stopTimer)
+          toast.addEventListener('mouseleave', MySwal.resumeTimer)
         }
       })
 
