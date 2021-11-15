65f7b0ece7d29bdedbba44a081bfc095aa3062bf
Emad Aldomari <ea@ea.sa>
2021-11-15 14:25:31 +0300

    get events from backend

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -1,40 +1,20 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import { Link } from "react-router-dom";
 
 export default function Events() {
+    const [events, setEvents] = useState([]);
+    useEffect(() => {
+        axios.get("/events").then(res => {
+            setEvents(res.data)
+        })
+    },[])
     return (
-       
+     
     <div className="flex flex-wrap pb-16 bg-white shadow overflow-hidden sm:rounded-lg">
-  <div className="w-full p-6 sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 mb-4 bg-gray-500">
-      <img className="rounded-lg" src="https://s3.ticketmx.com/uploads/images/ef7dcf538f9e4d9ea6e01bf3b813945857ab1d5b.jpeg?w=750&h=750&mode=crop&bgcolor=black&format=jpg" alt=""/>
-  </div>
-  <div className="w-full  p-6 sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 mb-4 bg-gray-500">
-      <img className="rounded-lg" src="https://s3.ticketmx.com/uploads/images/ef7dcf538f9e4d9ea6e01bf3b813945857ab1d5b.jpeg?w=750&h=750&mode=crop&bgcolor=black&format=jpg" alt=""/>
-  </div><div className="w-full  p-6 sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 mb-4 bg-gray-500">
-      <img className="rounded-lg" src="https://s3.ticketmx.com/uploads/images/ef7dcf538f9e4d9ea6e01bf3b813945857ab1d5b.jpeg?w=750&h=750&mode=crop&bgcolor=black&format=jpg" alt=""/>
-  </div><div className="w-full  p-6 sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 mb-4 bg-gray-500">
-      <img className="rounded-lg" src="https://s3.ticketmx.com/uploads/images/ef7dcf538f9e4d9ea6e01bf3b813945857ab1d5b.jpeg?w=750&h=750&mode=crop&bgcolor=black&format=jpg" alt=""/>
-  </div><div className="w-full  p-6 sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 mb-4 bg-gray-500">
-      <img className="rounded-lg" src="https://s3.ticketmx.com/uploads/images/ef7dcf538f9e4d9ea6e01bf3b813945857ab1d5b.jpeg?w=750&h=750&mode=crop&bgcolor=black&format=jpg" alt=""/>
-  </div><div className="w-full  p-6 sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 mb-4 bg-gray-500">
-      <img className="rounded-lg" src="https://s3.ticketmx.com/uploads/images/ef7dcf538f9e4d9ea6e01bf3b813945857ab1d5b.jpeg?w=750&h=750&mode=crop&bgcolor=black&format=jpg" alt=""/>
-  </div><div className="w-full  p-6 sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 mb-4 bg-gray-500">
-      <img className="rounded-lg" src="https://s3.ticketmx.com/uploads/images/ef7dcf538f9e4d9ea6e01bf3b813945857ab1d5b.jpeg?w=750&h=750&mode=crop&bgcolor=black&format=jpg" alt=""/>
-  </div>
-  <div className="w-full  p-6 sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 mb-4 bg-gray-500">
-      <img className="rounded-lg" src="https://s3.ticketmx.com/uploads/images/ef7dcf538f9e4d9ea6e01bf3b813945857ab1d5b.jpeg?w=750&h=750&mode=crop&bgcolor=black&format=jpg" alt=""/>
-  </div>
-  <div className="w-full  p-6 sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 mb-4 bg-gray-500">
-      <img className="rounded-lg" src="https://s3.ticketmx.com/uploads/images/ef7dcf538f9e4d9ea6e01bf3b813945857ab1d5b.jpeg?w=750&h=750&mode=crop&bgcolor=black&format=jpg" alt=""/>
-  </div>
-  <div className="w-full  p-6 sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 mb-4 bg-gray-500">
-      <img className="rounded-lg" src="https://s3.ticketmx.com/uploads/images/ef7dcf538f9e4d9ea6e01bf3b813945857ab1d5b.jpeg?w=750&h=750&mode=crop&bgcolor=black&format=jpg" alt=""/>
-  </div>
-  <div className="w-full  p-6 sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 mb-4 bg-gray-500">
-      <img className="rounded-lg" src="https://s3.ticketmx.com/uploads/images/ef7dcf538f9e4d9ea6e01bf3b813945857ab1d5b.jpeg?w=750&h=750&mode=crop&bgcolor=black&format=jpg" alt=""/>
-  </div>
-  <div className="w-full  p-6 sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 mb-4 bg-gray-500">
-      <img className="rounded-lg" src="https://s3.ticketmx.com/uploads/images/ef7dcf538f9e4d9ea6e01bf3b813945857ab1d5b.jpeg?w=750&h=750&mode=crop&bgcolor=black&format=jpg" alt=""/>
-  </div>
+  {events.map(event =>   <div className="w-full p-6 sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 mb-4 bg-gray-500">
+       <Link to={`/events/${event.id}`}><img className="rounded-lg" src={event.image} alt=""/></Link>
+   </div>)}
   </div>
         
     )
