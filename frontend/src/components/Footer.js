e4c5a80db94d63685ff352e3331c3073dac54281
Maha Alharbi <m.a.alharbi995@gmail.com>
2021-11-16 00:35:46 +0300

    footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -5,7 +5,7 @@ export default function Footer() {
         return (
           <div className="relative mt-16  bg-white">
             <svg
-              className="absolute top-0 w-full h-6 -mt-5 sm:-mt-10 sm:h-16 text-deep-purple-accent-400"
+              className="absolute top-0 w-full h-6 -mt-5 sm:-mt-10 sm:h-16 text-purple1"
               preserveAspectRatio="none"
               viewBox="0 0 1440 54"
             >
@@ -18,26 +18,26 @@ export default function Footer() {
               <div className="grid gap-16 row-gap-10 mb-8 lg:grid-cols-6">
                 <div className="md:max-w-md lg:col-span-2">
                 
-                    <span className="ml-2 text-xl font-bold tracking-wide text-gray-100 uppercase">
+                    <span className="ml-2 text-xl font-bold tracking-wide text-darkblue uppercase">
                      Riyadh Tikets
                     </span>
                  
                   <div className="mt-4 lg:max-w-sm">
-                    <p className="text-sm text-deep-purple-50">
+                    <p className="text-sm text-darkblue">
                     The Easiest Way to Find and Book Events.
                     </p>
                    
                   </div>
                 </div>
               </div>
-              <div className="flex flex-col justify-between pt-5 pb-10 border-t border-deep-purple-accent-200 sm:flex-row">
-                <p className="text-sm text-gray-100">
+              <div className="flex flex-col justify-between pt-5 pb-10  sm:flex-row">
+                <p className="text-sm text-darkblue">
                   © Copyright 2021 Riyadh Tickets. All rights reserved.
                 </p>
                 <div className="flex items-center mt-4 space-x-4 sm:mt-0">
                   <a
                     href="/"
-                    className="transition-colors duration-300 text-deep-purple-100 hover:text-teal-accent-400"
+                    className="transition-colors duration-300 text-darkblue hover:text-teal-accent-400"
                   >
                     <svg viewBox="0 0 24 24" fill="currentColor" className="h-5">
                       <path d="M24,4.6c-0.9,0.4-1.8,0.7-2.8,0.8c1-0.6,1.8-1.6,2.2-2.7c-1,0.6-2,1-3.1,1.2c-0.9-1-2.2-1.6-3.6-1.6 c-2.7,0-4.9,2.2-4.9,4.9c0,0.4,0,0.8,0.1,1.1C7.7,8.1,4.1,6.1,1.7,3.1C1.2,3.9,1,4.7,1,5.6c0,1.7,0.9,3.2,2.2,4.1 C2.4,9.7,1.6,9.5,1,9.1c0,0,0,0,0,0.1c0,2.4,1.7,4.4,3.9,4.8c-0.4,0.1-0.8,0.2-1.3,0.2c-0.3,0-0.6,0-0.9-0.1c0.6,2,2.4,3.4,4.6,3.4 c-1.7,1.3-3.8,2.1-6.1,2.1c-0.4,0-0.8,0-1.2-0.1c2.2,1.4,4.8,2.2,7.5,2.2c9.1,0,14-7.5,14-14c0-0.2,0-0.4,0-0.6 C22.5,6.4,23.3,5.5,24,4.6z" />
@@ -45,7 +45,7 @@ export default function Footer() {
                   </a>
                   <a
                     href="#"
-                    className="transition-colors duration-300 text-deep-purple-100 hover:text-teal-accent-400"
+                    className="transition-colors duration-300 text-darkblue hover:text-teal-accent-400"
                   >
                     <svg viewBox="0 0 30 30" fill="currentColor" className="h-6">
                       <circle cx="15" cy="15" r="4" />
@@ -54,9 +54,9 @@ export default function Footer() {
                   </a>
                   <a
                     href="#"
-                    className="transition-colors duration-300 text-deep-purple-100 hover:text-teal-accent-400"
+                    className="transition-colors duration-300 text-darkblue hover:text-teal-accent-400"
                   >
-                    <svg viewBox="0 0 24 24" fill="currentColor" className="h-5">
+                    <svg viewBox="0 0 24 24" fill="currentColor" className="h-5 ">
                       <path d="M22,0H2C0.895,0,0,0.895,0,2v20c0,1.105,0.895,2,2,2h11v-9h-3v-4h3V8.413c0-3.1,1.893-4.788,4.659-4.788 c1.325,0,2.463,0.099,2.795,0.143v3.24l-1.918,0.001c-1.504,0-1.795,0.715-1.795,1.763V11h4.44l-1,4h-3.44v9H22c1.105,0,2-0.895,2-2 V2C24,0.895,23.105,0,22,0z" />
                     </svg>
                   </a>
