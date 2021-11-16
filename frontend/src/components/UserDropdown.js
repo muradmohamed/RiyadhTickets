2136392053662d881c971448249972c547605ac7
Maha Alharbi <m.a.alharbi995@gmail.com>
2021-11-16 14:05:14 +0300

    link mytickets

--- frontend/src/components/UserDropdown.js
+++ frontend/src/components/UserDropdown.js
@@ -3,6 +3,8 @@ import { Fragment,useContext } from 'react'
 import { Menu, Transition } from '@headlessui/react'
 import { ChevronDownIcon } from '@heroicons/react/solid'
 import { ContextStore } from '../context'
+import { Link } from "react-router-dom";
+
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(' ')
@@ -35,15 +37,15 @@ export default function UserDropdown() {
           <div className="py-1">
             <Menu.Item>
               {({ active }) => (
-                <a
-                  href="#"
+                <Link
+                  to="/mytickets"
                   className={classNames(
                     active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
                     'block px-4 py-2 text-sm'
                   )}
                 >
                   My Tickets
-                </a>
+                </Link>
               )}
             </Menu.Item>
             <form method="POST" action="#">
