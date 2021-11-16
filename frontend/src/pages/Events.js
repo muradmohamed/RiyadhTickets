c5023c4b56e4834152bb2a20b627801faa22f871
Maha Alharbi <m.a.alharbi995@gmail.com>
2021-11-16 23:14:03 +0300

    improvements

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -1,54 +1,56 @@
-import axios from 'axios'
-import React, { useEffect, useState } from 'react'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function Events() {
-    const [events, setEvents] = useState([]);
-    useEffect(() => {
-        axios.get("/events").then(res => {
-            setEvents(res.data)
-        })
-    },[])
-    return (
+  const [events, setEvents] = useState([]);
+  useEffect(() => {
+    axios.get("/events").then((res) => {
+      setEvents(res.data);
+    });
+  }, []);
 
-        <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
-        <div className="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
-          <div>
-            <p className="inline-block px-3 py-px mb-4 text-xs font-semibold tracking-wider  text-purple1 uppercase rounded-full bg-teal-accent-400">
-              Events
-            </p>
-          </div>
-          <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:mx-auto">
-            <span className="relative inline-block">
-              <svg
-                viewBox="0 0 52 24"
-                fill="currentColor"
-                className="absolute top-0 left-0 z-0 hidden w-32 -mt-8 -ml-20  text-purpule2 lg:w-32 lg:-ml-28 lg:-mt-10 sm:block"
-              >
-                <defs>
-                  <pattern
-                    id="1d4040f3-9f3e-4ac7-b117-7d4009658ced"
-                    x="0"
-                    y="0"
-                    width=".135"
-                    height=".30"
-                  >
-                    <circle cx="1" cy="1" r=".7" />
-                  </pattern>
-                </defs>
-                <rect
-                  fill="url(#1d4040f3-9f3e-4ac7-b117-7d4009658ced)"
-                  width="52"
-                  height="24"
-                />
-              </svg>
-              <span className="relative text-darkblue ">Discover The Latest Events </span>
-            </span>
-          </h2>
-        
+  return (
+    <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+      <div className="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
+        <div>
+          <p className="inline-block px-3 py-px mb-4 text-xs font-semibold tracking-wider  text-purple1 uppercase rounded-full bg-graylight">
+            Events
+          </p>
         </div>
-        <div className="grid gap-10 sm:grid-cols-2 lg:grid-cols-4">
-          {events.map((event) => <Link to={`/events/${event.id}`}>
+        <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold leading-none tracking-tight  sm:text-4xl md:mx-auto">
+          <span className="relative inline-block">
+            <svg
+              viewBox="0 0 52 24"
+              fill="currentColor"
+              className="absolute top-0 left-0 z-0 hidden w-32 -mt-8 -ml-20  text-purpule2 lg:w-32 lg:-ml-28 lg:-mt-10 sm:block"
+            >
+              <defs>
+                <pattern
+                  id="1d4040f3-9f3e-4ac7-b117-7d4009658ced"
+                  x="0"
+                  y="0"
+                  width=".135"
+                  height=".30"
+                >
+                  <circle cx="1" cy="1" r=".7" />
+                </pattern>
+              </defs>
+              <rect
+                fill="url(#1d4040f3-9f3e-4ac7-b117-7d4009658ced)"
+                width="52"
+                height="24"
+              />
+            </svg>
+            <span className="relative text-darkblue ">
+              Discover The Latest Events{" "}
+            </span>
+          </span>
+        </h2>
+      </div>
+      <div className="grid gap-10 sm:grid-cols-2 lg:grid-cols-4">
+        {events.map((event) => (
+          <Link to={`/events/${event.id}`}>
             <div className="relative cursor-pointer overflow-hidden transition duration-300 transform rounded shadow-lg lg:hover:-translate-y-2 hover:shadow-2xl">
               <img
                 className="object-cover w-full h-56 md:h-64 xl:h-80"
@@ -58,16 +60,14 @@ export default function Events() {
               <div className="absolute inset-0 flex flex-col justify-center px-5 py-4 text-center transition-opacity duration-300 bg-black bg-opacity-75 opacity-0 hover:opacity-100">
                 <p className="mb-4 text-xl text-gray-50">{event.title}</p>
                 <p className="mb-4 text-md tracking-wide text-gray-50">
-                {event.start_date}
+                  {event.start_date}
                 </p>
-                <div className="flex items-center justify-center space-x-3">
-                </div>
+                <div className="flex items-center justify-center space-x-3"></div>
               </div>
             </div>
-          </Link>)}
-
-
-        </div>
-      </div>  
-    )
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
 }
--- frontend/src/pages/Reservation.js
+++ frontend/src/pages/Reservation.js
@@ -1,49 +1,44 @@
-import React from 'react'
-import {useState, useEffect} from 'react'
-import axios from 'axios';
-import { useNavigate, useParams } from 'react-router-dom';
-import Swal from 'sweetalert2';
+import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { useNavigate, useParams } from "react-router-dom";
+import Swal from "sweetalert2";
 
 export default function Reservation() {
   const navigate = useNavigate();
-    const {id}= useParams();
-    const[eventsDetails, setEventsDetails] = useState({});
-    useEffect(() => { 
-        axios.get(`/events/${id}`)
-        .then((res)=>{
-            console.log(res.data)
-       setEventsDetails(res.data)
-        })
- 
-     },[])
+  const { id } = useParams();
+  const [eventsDetails, setEventsDetails] = useState({});
+  useEffect(() => {
+    axios.get(`/events/${id}`).then((res) => {
+      console.log(res.data);
+      setEventsDetails(res.data);
+    });
+  }, []);
 
-     const book =(key)=> {
+  const book = (key) => {
+    axios.post(`/events/${id}/book`, { class: key }).then((res) => {
+      Swal.fire({
+        title: "You Successfully Created Your Booking",
+        text: "Redirecting to ticket .....",
+        icon: "success",
+        showCancelButton: false,
+        showConfirmButton: false,
+        timer: 1500,
+        timerProgressBar: true,
+        didClose: () => {
+          navigate("/mytickets");
+        },
+      });
+    });
+  };
+  if (!Object.keys(eventsDetails).length) return <div>Loading</div>;
 
-        axios.post(`/events/${id}/book` , {class: key})
-        .then((res)=>{
-           Swal.fire({
-               title: 'You Successfully Created Your Booking',
-               text: 'Redirecting to ticket .....',
-               icon: 'success',
-               showCancelButton: false,
-               showConfirmButton: false,
-               timer: 1500,
-               timerProgressBar: true,
-               didClose: () => {
-                navigate("/mytickets");
-               }
-           } 
-           )
-        })
-     }
-   if(!Object.keys(eventsDetails).length ) return <div>Loading</div>
-
-    return (
-        <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+  return (
+    <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
       <div className="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
         <div>
           <p className="inline-block px-3 py-px mb-4 text-xs font-semibold tracking-wider text-purple1 uppercase rounded-full bg-teal-accent-400">
-           BOOKING
+            BOOKING
           </p>
         </div>
         <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-darkblue sm:text-4xl md:mx-auto">
@@ -71,26 +66,29 @@ export default function Reservation() {
               />
             </svg>
             <span className="relative">Ticket Class For The Event</span>
-          </span> 
+          </span>
         </h2>
-        <p className=' text-darkblue'>Select Your Ticket Class</p>
+        <p className=" text-darkblue">Select Your Ticket Class</p>
       </div>
       <div className="grid gap-8 row-gap-5 md:row-gap-8 lg:grid-cols-3">
-      {Object.keys(eventsDetails.prices).map((key)=>
-        <div className="p-5 cursor-pointer duration-300 transform bg-white
+        {Object.keys(eventsDetails.prices).map((key) => (
+          <div
+            className="p-5 cursor-pointer duration-300 transform bg-white
          border-2 border-dashed rounded shadow-sm  border-purple1
-          hover:-translate-y-2"  onClick={()=> book(key) }>
-          <div className="flex items-center mb-2 text-center">
-            
-            <p className="text-lg font-bold text-center text-darkblue ">{key}</p>
+          hover:-translate-y-2"
+            onClick={() => book(key)}
+          >
+            <div className="flex items-center mb-2 text-center">
+              <p className="text-lg font-bold text-center text-darkblue ">
+                {key}
+              </p>
+            </div>
+            <p className="text-sm text-darkblue text-center font-bold">
+              {eventsDetails.prices[key]} SAR
+            </p>
           </div>
-          <p className="text-sm text-darkblue text-center font-bold">
-          {eventsDetails.prices[key]} SAR
-          </p>
-        </div>
-      )}    
+        ))}
       </div>
     </div>
-    )
+  );
 }
-
