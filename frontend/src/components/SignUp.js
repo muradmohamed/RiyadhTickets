2b011edb8863b6081073915e1a6d83930aa69267
Maha Alharbi <m.a.alharbi995@gmail.com>
2021-11-16 23:11:11 +0300

    responsive navbar

--- frontend/src/components/SignUp.js
+++ frontend/src/components/SignUp.js
@@ -1,131 +1,143 @@
 import { useContext, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { showSignup } from '../reducers/assets';
-import axios from 'axios';
+import { showSignup } from "../reducers/assets";
+import axios from "axios";
 import { ContextStore } from "../context";
 
 export default function SignUp() {
-    const state = useSelector((state) => ({...state.assets}));
-    const {Toast, setToken} = useContext(ContextStore);
-    const dispatch = useDispatch();
-    const [email, setEmail] = useState("");
-    const [password, setPass] = useState("");
-    const [name, setName] = useState("");
+  const state = useSelector((state) => ({ ...state.assets }));
+  const { Toast, setToken } = useContext(ContextStore);
+  const dispatch = useDispatch();
+  const [email, setEmail] = useState("");
+  const [password, setPass] = useState("");
+  const [name, setName] = useState("");
 
-    const signup = () => {
-      axios.post('/user/register', {email, name, password}).then((res) => {
+  const signup = () => {
+    axios
+      .post("/user/register", { email, name, password })
+      .then((res) => {
         setToken(`Bearer ${res.data.token}`);
         dispatch(showSignup());
         Toast.fire({
-          icon: 'success',
-          title: 'Signed up successfully'
-        });
-      }).catch((err) => {
-        Toast.fire({
-          icon: 'error',
-          title: err.response.data.error
+          icon: "success",
+          title: "Signed up successfully",
         });
       })
-      
-    }
-    return (
-        <div>
-            {state.showSignup ? (
-            <>
-           
-          <div
-            className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none bg-black bg-opacity-30 focus:outline-none"
-          >
+      .catch((err) => {
+        Toast.fire({
+          icon: "error",
+          title: err.response.data.error,
+        });
+      });
+  };
+  return (
+    <div>
+      {state.showSignup ? (
+        <>
+          <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none bg-black bg-opacity-30 focus:outline-none">
             <div className="relative w-auto my-6 mx-auto ">
               {/*content*/}
               <div className="border-0  rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
                 {/*header*/}
-                <div className=" flex items-start justify-items-center  p-5 border-b border-solid border-blueGray-200 rounded-t">
+                <div className=" flex items-start justify-items-center  p-5 border-b border-solid  border-graylight rounded-t">
                   <h3
-                  style={{width: "15rem"}}
-                  className="justify-self-start text-3xl text-purple1 font-semibold">
-                   Sign Up
+                    style={{ width: "15rem" }}
+                    className="justify-self-start text-3xl  text-darkblue font-semibold"
+                  >
+                    Sign Up
                   </h3>
                   <button
-                  
                     className="justify-self-end p-1 ml-auto bg-transparent border-0 text-darkblue opacity-30 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
                     onClick={() => dispatch(showSignup())}
-                  > <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6  " fill="none" viewBox="0 0 24 24" stroke="currentColor">
-  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
-</svg></button>
+                  >
+                    
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-6 w-6  "
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth={2}
+                        d="M6 18L18 6M6 6l12 12"
+                      />
+                    </svg>
+                  </button>
                 </div>
 
-                <form className="relative p-6 flex-auto" >
-                <div className="relative w-full mb-3">
-                        <label
-                          className="block uppercase text-darkblue text-xs font-bold mb-2"
-                          htmlFor="grid-password"
-                        >
-                          Name
-                        </label>
-                        <input
-                          type="text"
-                          value={name}
-                          onChange={(e) => setName(e.target.value)}
-                          className="border-0 px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full"
-                          placeholder="Name"
-                          style={{ transition: "all .15s ease" }}
-                        />
-                      </div>
+                <form className="relative p-6 flex-auto">
+                  <div className="relative w-full mb-3">
+                    <label
+                      className="block uppercase text-darkblue text-xs font-bold mb-2"
+                      htmlFor="grid-password"
+                    >
+                      Name
+                    </label>
+                    <input
+                      type="text"
+                      value={name}
+                      onChange={(e) => setName(e.target.value)}
+                      className="border-0 px-3 py-3 placeholder-graylight  bg-white rounded text-sm shadow focus:outline-none focus:ring w-full"
+                      placeholder="Name"
+                      style={{ transition: "all .15s ease" }}
+                    />
+                  </div>
 
+                  <div className="relative w-full mb-3">
+                    <label
+                      className="block uppercase text-darkblue text-xs font-bold mb-2"
+                      htmlFor="grid-password"
+                    >
+                      Email
+                    </label>
+                    <input
+                      type="email"
+                      value={email}
+                      onChange={(e) => setEmail(e.target.value)}
+                      className="border-0 px-3 py-3 placeholder-graylight  bg-white rounded text-sm shadow focus:outline-none focus:ring w-full"
+                      placeholder="Email"
+                      style={{ transition: "all .15s ease" }}
+                    />
+                  </div>
 
-                      <div className="relative w-full mb-3">
-                        <label
-                          className="block uppercase text-darkblue text-xs font-bold mb-2"
-                          htmlFor="grid-password"
-                        >
-                          Email
-                        </label>
-                        <input
-                          type="email"
-                          value={email}
-                          onChange={(e) => setEmail(e.target.value)}
-                          className="border-0 px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full"
-                          placeholder="Email"
-                          style={{ transition: "all .15s ease" }}
-                        />
-                      </div>
+                  <div className="relative w-full mb-3">
+                    <label
+                      className="block uppercase text-darkblue text-xs font-bold mb-2"
+                      htmlFor="grid-password"
+                    >
+                      Password
+                    </label>
+                    <input
+                      type="password"
+                      value={password}
+                      onChange={(e) => setPass(e.target.value)}
+                      className="border-0 px-3 py-3 placeholder-graylight bg-white rounded text-sm shadow focus:outline-none focus:ring w-full"
+                      placeholder="Password"
+                      style={{ transition: "all .15s ease" }}
+                    />
+                  </div>
 
-                      <div className="relative w-full mb-3">
-                        <label
-                          className="block uppercase text-darkblue text-xs font-bold mb-2"
-                          htmlFor="grid-password"
-                        >
-                          Password
-                        </label>
-                        <input
-                          type="password"
-                          value={password}
-                          onChange={(e) => setPass(e.target.value)}
-                          className="border-0 px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full"
-                          placeholder="Password"
-                          style={{ transition: "all .15s ease" }}
-                        />
-                      </div>
-                    
-                      <div className="text-center mt-6">
-                        <button
-                          className="bg-gray-900 text-white active: bg-purple1 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full"
-                          type="button"
-                          style={{ transition: "all .15s ease" }}
-                          onClick={() =>  signup()}  >
-                          Sign Up
-                        </button>
-                      </div>
-                    </form>
+                  <div className="text-center mt-6">
+                    <button
+                      className="bg-orang1 text-white  text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg hover:bg-orang2 outline-none focus:outline-none mr-1 mb-1 w-full"
+                      type="button"
+                      style={{ transition: "all .15s ease" }}
+                      onClick={() => signup()}
+                    >
+                      Sign Up
+                    </button>
+                  </div>
+                </form>
                 {/*footer*/}
-           
               </div>
             </div>
           </div>
           <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
-        </> 
-         ) : null}
-        </div>
-    )
+        </>
+      ) : null}
+    </div>
+  );
 }
--- frontend/src/components/UserDropdown.js
+++ frontend/src/components/UserDropdown.js
@@ -1,27 +1,42 @@
 /* This example requires Tailwind CSS v2.0+ */
-import { Fragment,useContext } from 'react'
-import { Menu, Transition } from '@headlessui/react'
-import { ChevronDownIcon } from '@heroicons/react/solid'
-import { ContextStore } from '../context'
+import { Fragment, useContext } from "react";
+import { Menu, Transition } from "@headlessui/react";
+import { ChevronDownIcon } from "@heroicons/react/solid";
+import { ContextStore } from "../context";
 import { Link } from "react-router-dom";
 
-
 function classNames(...classes) {
-  return classes.filter(Boolean).join(' ')
+  return classes.filter(Boolean).join(" ");
 }
 export default function UserDropdown() {
-  const {user, setToken} = useContext(ContextStore);  
+  const { user, setToken } = useContext(ContextStore);
   return (
     <Menu as="a" className="p-4 ">
       <>
-        <Menu.Button as="a" className="inline cursor-pointer">
-        <svg xmlns="http://www.w3.org/2000/svg" className="mr-2 mb-1 ml-2 h-5 w-5 inline" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
-</svg>  Hello {user.name} 
-          <ChevronDownIcon className="inline mb-1 -mr-1 ml-2 h-5 w-5" aria-hidden="true" />
+        <Menu.Button
+          as="a"
+          className="inline cursor-pointer text-purple1 hover:text-darkpurple"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="mr-2 mb-1 ml-2 h-5 w-5 inline"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+            />
+          </svg>{" "}
+          Hello {user.name}
+          <ChevronDownIcon
+            className="inline mb-1 -mr-1 ml-2 h-5 w-5"
+            aria-hidden="true"
+          />
         </Menu.Button>
-
-
       </>
 
       <Transition
@@ -33,15 +48,15 @@ export default function UserDropdown() {
         leaveFrom="transform opacity-100 scale-100"
         leaveTo="transform opacity-0 scale-95"
       >
-        <Menu.Items className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
+        <Menu.Items className="origin-top-right absolute text-purple1 right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
           <div className="py-1">
             <Menu.Item>
               {({ active }) => (
                 <Link
                   to="/mytickets"
                   className={classNames(
-                    active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                    'block px-4 py-2 text-sm'
+                    active ? "bg-graylight" : " text-purple1",
+                    "block px-4 py-2 text-sm"
                   )}
                 >
                   My Tickets
@@ -51,13 +66,13 @@ export default function UserDropdown() {
             <form method="POST" action="#">
               <Menu.Item>
                 {({ active }) => (
-                  <button   
-                  onClick={() => setToken(undefined)}
+                  <button
+                    onClick={() => setToken(undefined)}
                     type="submit"
                     className={classNames(
-                      active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                      'block w-full text-left px-4 py-2 text-sm'
-                    )} 
+                      active ? "bg-graylight" : " text-purple1",
+                      "block w-full text-left px-4 py-2 text-sm"
+                    )}
                   >
                     Sign out
                   </button>
@@ -68,5 +83,5 @@ export default function UserDropdown() {
         </Menu.Items>
       </Transition>
     </Menu>
-  )
+  );
 }
