e68abb1b0c4ba6adb8afaef88bb96b5eab79b7a1
Maha Alharbi <m.a.alharbi995@gmail.com>
2021-11-16 08:56:58 +0300

    Some changes on the Events

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -2,40 +2,271 @@ import React from 'react'
 
 export default function Events() {
     return (
+        <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+        <div className="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
+          <div>
+            <p className="inline-block px-3 py-px mb-4 text-xs font-semibold tracking-wider  text-purple1 uppercase rounded-full bg-teal-accent-400">
+              Events
+            </p>
+          </div>
+          <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:mx-auto">
+            <span className="relative inline-block">
+              <svg
+                viewBox="0 0 52 24"
+                fill="currentColor"
+                className="absolute top-0 left-0 z-0 hidden w-32 -mt-8 -ml-20  text-purpule2 lg:w-32 lg:-ml-28 lg:-mt-10 sm:block"
+              >
+                <defs>
+                  <pattern
+                    id="1d4040f3-9f3e-4ac7-b117-7d4009658ced"
+                    x="0"
+                    y="0"
+                    width=".135"
+                    height=".30"
+                  >
+                    <circle cx="1" cy="1" r=".7" />
+                  </pattern>
+                </defs>
+                <rect
+                  fill="url(#1d4040f3-9f3e-4ac7-b117-7d4009658ced)"
+                  width="52"
+                  height="24"
+                />
+              </svg>
+              <span className="relative text-darkblue">Discover the latest events </span>
+            </span>
+          </h2>
+        
+        </div>
+        <div className="grid gap-10 sm:grid-cols-2 lg:grid-cols-4">
+          <div>
+            <div className="relative overflow-hidden transition duration-300 transform rounded shadow-lg lg:hover:-translate-y-2 hover:shadow-2xl">
+              <img
+                className="object-cover w-full h-56 md:h-64 xl:h-80"
+                src="https://s3.ticketmx.com/uploads/images/25cd9ef5ad0e7ec032b0b000bdc51a740fabbabb.png?w=750&h=750&mode=crop&bgcolor=black&format=jpg"
+                alt="event"
+              />
+              <div className="absolute inset-0 flex flex-col justify-center px-5 py-4 text-center transition-opacity duration-300 bg-black bg-opacity-75 opacity-0 hover:opacity-100">
+                <p className="mb-4 text-xl text-gray-50">RIYADH BLVD CITY</p>
+                <p className="mb-4 text-md tracking-wide text-gray-50">
+                01 November 2021
+                </p>
+                <div className="flex items-center justify-center space-x-3">
+            
+                </div>
+              </div>
+            </div>
+          </div>
+          <div>
+            <div className="relative overflow-hidden transition duration-300 transform rounded shadow-lg lg:hover:-translate-y-2 hover:shadow-2xl">
+              <img
+                className="object-cover w-full h-56 md:h-64 xl:h-80"
+                src="https://s3.ticketmx.com/uploads/images/25cd9ef5ad0e7ec032b0b000bdc51a740fabbabb.png?w=750&h=750&mode=crop&bgcolor=black&format=jpg"
+                alt="event"
+              />
+              <div className="absolute inset-0 flex flex-col justify-center px-5 py-4 text-center transition-opacity duration-300 bg-black bg-opacity-75 opacity-0 hover:opacity-100">
+                <p className="mb-4 text-xl text-gray-50">RIYADH BLVD CITY</p>
+                <p className="mb-4 text-md tracking-wide text-gray-50">
+                01 November 2021
+                </p>
+                <div className="flex items-center justify-center space-x-3">
+                 
+                </div>
+              </div>
+            </div>
+          </div>
+          {/*  */}
+          <div>
+            <div className="relative overflow-hidden transition duration-300 transform rounded shadow-lg lg:hover:-translate-y-2 hover:shadow-2xl">
+              <img
+                className="object-cover w-full h-56 md:h-64 xl:h-80"
+                src="https://s3.ticketmx.com/uploads/images/25cd9ef5ad0e7ec032b0b000bdc51a740fabbabb.png?w=750&h=750&mode=crop&bgcolor=black&format=jpg"
+                alt="event"
+              />
+              <div className="absolute inset-0 flex flex-col justify-center px-5 py-4 text-center transition-opacity duration-300 bg-black bg-opacity-75 opacity-0 hover:opacity-100">
+                <p className="mb-4 text-xl text-gray-50">RIYADH BLVD CITY</p>
+                <p className="mb-4 text-md tracking-wide text-gray-50">
+                01 November 2021
+                </p>
+                <div className="flex items-center justify-center space-x-3">
+                 
+                </div>
+              </div>
+            </div>
+          </div>
+          {/*  */}
+
+          <div>
+            <div className="relative overflow-hidden transition duration-300 transform rounded shadow-lg lg:hover:-translate-y-2 hover:shadow-2xl">
+              <img
+                className="object-cover w-full h-56 md:h-64 xl:h-80"
+                src="https://s3.ticketmx.com/uploads/images/25cd9ef5ad0e7ec032b0b000bdc51a740fabbabb.png?w=750&h=750&mode=crop&bgcolor=black&format=jpg"
+                alt="event"
+              />
+              <div className="absolute inset-0 flex flex-col justify-center px-5 py-4 text-center transition-opacity duration-300 bg-black bg-opacity-75 opacity-0 hover:opacity-100">
+                <p className="mb-4 text-xl text-gray-50">RIYADH BLVD CITY</p>
+                <p className="mb-4 text-md tracking-wide text-gray-50">
+                01 November 2021
+                </p>
+                <div className="flex items-center justify-center space-x-3">
+                  
+                </div>
+              </div>
+            </div>
+          </div>
+          {/*  */}
+          <div>
+            <div className="relative overflow-hidden transition duration-300 transform rounded shadow-lg lg:hover:-translate-y-2 hover:shadow-2xl">
+              <img
+                className="object-cover w-full h-56 md:h-64 xl:h-80"
+                src="https://s3.ticketmx.com/uploads/images/25cd9ef5ad0e7ec032b0b000bdc51a740fabbabb.png?w=750&h=750&mode=crop&bgcolor=black&format=jpg"
+                alt="event"
+              />
+              <div className="absolute inset-0 flex flex-col justify-center px-5 py-4 text-center transition-opacity duration-300 bg-black bg-opacity-75 opacity-0 hover:opacity-100">
+                <p className="mb-4 text-xl text-gray-50">RIYADH BLVD CITY</p>
+                <p className="mb-4 text-md tracking-wide text-gray-50">
+                01 November 2021
+                </p>
+                <div className="flex items-center justify-center space-x-3">
+                
+                </div>
+              </div>
+            </div>
+          </div>
+          <div>
+            <div className="relative overflow-hidden transition duration-300 transform rounded shadow-lg lg:hover:-translate-y-2 hover:shadow-2xl">
+              <img
+                className="object-cover w-full h-56 md:h-64 xl:h-80"
+                src="https://s3.ticketmx.com/uploads/images/25cd9ef5ad0e7ec032b0b000bdc51a740fabbabb.png?w=750&h=750&mode=crop&bgcolor=black&format=jpg"
+                alt="event"
+              />
+              <div className="absolute inset-0 flex flex-col justify-center px-5 py-4 text-center transition-opacity duration-300 bg-black bg-opacity-75 opacity-0 hover:opacity-100">
+                <p className="mb-4 text-xl text-gray-50">RIYADH BLVD CITY</p>
+                <p className="mb-4 text-md tracking-wide text-gray-50">
+                01 November 2021
+                </p>
+                <div className="flex items-center justify-center space-x-3">
+                
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div>
+            <div className="relative overflow-hidden transition duration-300 transform rounded shadow-lg lg:hover:-translate-y-2 hover:shadow-2xl">
+              <img
+                className="object-cover w-full h-56 md:h-64 xl:h-80"
+                src="https://s3.ticketmx.com/uploads/images/25cd9ef5ad0e7ec032b0b000bdc51a740fabbabb.png?w=750&h=750&mode=crop&bgcolor=black&format=jpg"
+                alt="event"
+              />
+              <div className="absolute inset-0 flex flex-col justify-center px-5 py-4 text-center transition-opacity duration-300 bg-black bg-opacity-75 opacity-0 hover:opacity-100">
+                <p className="mb-4 text-xl text-gray-50">RIYADH BLVD CITY</p>
+                <p className="mb-4 text-md tracking-wide text-gray-50">
+                01 November 2021
+                </p>
+                <div className="flex items-center justify-center space-x-3">
+                  
+                </div>
+              </div>
+            </div>
+          </div>
+          <div>
+            <div className="relative overflow-hidden transition duration-300 transform rounded shadow-lg lg:hover:-translate-y-2 hover:shadow-2xl">
+              <img
+                className="object-cover w-full h-56 md:h-64 xl:h-80"
+                src="https://s3.ticketmx.com/uploads/images/25cd9ef5ad0e7ec032b0b000bdc51a740fabbabb.png?w=750&h=750&mode=crop&bgcolor=black&format=jpg"
+                alt="event"
+              />
+              <div className="absolute inset-0 flex flex-col justify-center px-5 py-4 text-center transition-opacity duration-300 bg-black bg-opacity-75 opacity-0 hover:opacity-100">
+                <p className="mb-4 text-xl text-gray-50">RIYADH BLVD CITY</p>
+                <p className="mb-4 text-md tracking-wide text-gray-50">
+                01 November 2021
+                </p>
+                <div className="flex items-center justify-center space-x-3">
+                  
+                </div>
+              </div>
+            </div>
+          </div>
+          <div>
+            <div className="relative overflow-hidden transition duration-300 transform rounded shadow-lg lg:hover:-translate-y-2 hover:shadow-2xl">
+              <img
+                className="object-cover w-full h-56 md:h-64 xl:h-80"
+                src="https://s3.ticketmx.com/uploads/images/25cd9ef5ad0e7ec032b0b000bdc51a740fabbabb.png?w=750&h=750&mode=crop&bgcolor=black&format=jpg"
+                alt="event"
+              />
+              <div className="absolute inset-0 flex flex-col justify-center px-5 py-4 text-center transition-opacity duration-300 bg-black bg-opacity-75 opacity-0 hover:opacity-100">
+                <p className="mb-4 text-xl text-gray-50">RIYADH BLVD CITY</p>
+                <p className="mb-4 text-md tracking-wide text-gray-50">
+                01 November 2021
+                </p>
+                <div className="flex items-center justify-center space-x-3">
+                 
+                </div>
+              </div>
+            </div>
+          </div>
+          <div>
+            <div className="relative overflow-hidden transition duration-300 transform rounded shadow-lg lg:hover:-translate-y-2 hover:shadow-2xl">
+              <img
+                className="object-cover w-full h-56 md:h-64 xl:h-80"
+                src="https://s3.ticketmx.com/uploads/images/25cd9ef5ad0e7ec032b0b000bdc51a740fabbabb.png?w=750&h=750&mode=crop&bgcolor=black&format=jpg"
+                alt="event"
+              />
+              <div className="absolute inset-0 flex flex-col justify-center px-5 py-4 text-center transition-opacity duration-300 bg-black bg-opacity-75 opacity-0 hover:opacity-100">
+                <p className="mb-4 text-xl text-gray-50">RIYADH BLVD CITY</p>
+                <p className="mb-4 text-md tracking-wide text-gray-50">
+                01 November 2021
+                </p>
+                <div className="flex items-center justify-center space-x-3">
+                  
+                </div>
+              </div>
+            </div>
+          </div>
+
+
+
+        </div>
+      </div>
+
+
+
+
        
-    <div className="flex flex-wrap pb-16 bg-white shadow overflow-hidden sm:rounded-lg">
-  <div className="w-full p-6 sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 mb-4 bg-gray-500">
-      <img className="rounded-lg" src="https://s3.ticketmx.com/uploads/images/ef7dcf538f9e4d9ea6e01bf3b813945857ab1d5b.jpeg?w=750&h=750&mode=crop&bgcolor=black&format=jpg" alt=""/>
-  </div>
-  <div className="w-full  p-6 sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 mb-4 bg-gray-500">
-      <img className="rounded-lg" src="https://s3.ticketmx.com/uploads/images/ef7dcf538f9e4d9ea6e01bf3b813945857ab1d5b.jpeg?w=750&h=750&mode=crop&bgcolor=black&format=jpg" alt=""/>
-  </div><div className="w-full  p-6 sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 mb-4 bg-gray-500">
-      <img className="rounded-lg" src="https://s3.ticketmx.com/uploads/images/ef7dcf538f9e4d9ea6e01bf3b813945857ab1d5b.jpeg?w=750&h=750&mode=crop&bgcolor=black&format=jpg" alt=""/>
-  </div><div className="w-full  p-6 sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 mb-4 bg-gray-500">
-      <img className="rounded-lg" src="https://s3.ticketmx.com/uploads/images/ef7dcf538f9e4d9ea6e01bf3b813945857ab1d5b.jpeg?w=750&h=750&mode=crop&bgcolor=black&format=jpg" alt=""/>
-  </div><div className="w-full  p-6 sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 mb-4 bg-gray-500">
-      <img className="rounded-lg" src="https://s3.ticketmx.com/uploads/images/ef7dcf538f9e4d9ea6e01bf3b813945857ab1d5b.jpeg?w=750&h=750&mode=crop&bgcolor=black&format=jpg" alt=""/>
-  </div><div className="w-full  p-6 sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 mb-4 bg-gray-500">
-      <img className="rounded-lg" src="https://s3.ticketmx.com/uploads/images/ef7dcf538f9e4d9ea6e01bf3b813945857ab1d5b.jpeg?w=750&h=750&mode=crop&bgcolor=black&format=jpg" alt=""/>
-  </div><div className="w-full  p-6 sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 mb-4 bg-gray-500">
-      <img className="rounded-lg" src="https://s3.ticketmx.com/uploads/images/ef7dcf538f9e4d9ea6e01bf3b813945857ab1d5b.jpeg?w=750&h=750&mode=crop&bgcolor=black&format=jpg" alt=""/>
-  </div>
-  <div className="w-full  p-6 sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 mb-4 bg-gray-500">
-      <img className="rounded-lg" src="https://s3.ticketmx.com/uploads/images/ef7dcf538f9e4d9ea6e01bf3b813945857ab1d5b.jpeg?w=750&h=750&mode=crop&bgcolor=black&format=jpg" alt=""/>
-  </div>
-  <div className="w-full  p-6 sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 mb-4 bg-gray-500">
-      <img className="rounded-lg" src="https://s3.ticketmx.com/uploads/images/ef7dcf538f9e4d9ea6e01bf3b813945857ab1d5b.jpeg?w=750&h=750&mode=crop&bgcolor=black&format=jpg" alt=""/>
-  </div>
-  <div className="w-full  p-6 sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 mb-4 bg-gray-500">
-      <img className="rounded-lg" src="https://s3.ticketmx.com/uploads/images/ef7dcf538f9e4d9ea6e01bf3b813945857ab1d5b.jpeg?w=750&h=750&mode=crop&bgcolor=black&format=jpg" alt=""/>
-  </div>
-  <div className="w-full  p-6 sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 mb-4 bg-gray-500">
-      <img className="rounded-lg" src="https://s3.ticketmx.com/uploads/images/ef7dcf538f9e4d9ea6e01bf3b813945857ab1d5b.jpeg?w=750&h=750&mode=crop&bgcolor=black&format=jpg" alt=""/>
-  </div>
-  <div className="w-full  p-6 sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 mb-4 bg-gray-500">
-      <img className="rounded-lg" src="https://s3.ticketmx.com/uploads/images/ef7dcf538f9e4d9ea6e01bf3b813945857ab1d5b.jpeg?w=750&h=750&mode=crop&bgcolor=black&format=jpg" alt=""/>
-  </div>
-  </div>
+//     <div className="flex flex-wrap pb-16 bg-white shadow overflow-hidden sm:rounded-lg">
+//   <div className="w-full p-6 sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 mb-4 bg-gray-500">
+//       <img className="rounded-lg" src="https://s3.ticketmx.com/uploads/images/ef7dcf538f9e4d9ea6e01bf3b813945857ab1d5b.jpeg?w=750&h=750&mode=crop&bgcolor=black&format=jpg" alt=""/>
+//   </div>
+//   <div className="w-full  p-6 sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 mb-4 bg-gray-500">
+//       <img className="rounded-lg" src="https://s3.ticketmx.com/uploads/images/ef7dcf538f9e4d9ea6e01bf3b813945857ab1d5b.jpeg?w=750&h=750&mode=crop&bgcolor=black&format=jpg" alt=""/>
+//   </div><div className="w-full  p-6 sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 mb-4 bg-gray-500">
+//       <img className="rounded-lg" src="https://s3.ticketmx.com/uploads/images/ef7dcf538f9e4d9ea6e01bf3b813945857ab1d5b.jpeg?w=750&h=750&mode=crop&bgcolor=black&format=jpg" alt=""/>
+//   </div><div className="w-full  p-6 sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 mb-4 bg-gray-500">
+//       <img className="rounded-lg" src="https://s3.ticketmx.com/uploads/images/ef7dcf538f9e4d9ea6e01bf3b813945857ab1d5b.jpeg?w=750&h=750&mode=crop&bgcolor=black&format=jpg" alt=""/>
+//   </div><div className="w-full  p-6 sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 mb-4 bg-gray-500">
+//       <img className="rounded-lg" src="https://s3.ticketmx.com/uploads/images/ef7dcf538f9e4d9ea6e01bf3b813945857ab1d5b.jpeg?w=750&h=750&mode=crop&bgcolor=black&format=jpg" alt=""/>
+//   </div><div className="w-full  p-6 sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 mb-4 bg-gray-500">
+//       <img className="rounded-lg" src="https://s3.ticketmx.com/uploads/images/ef7dcf538f9e4d9ea6e01bf3b813945857ab1d5b.jpeg?w=750&h=750&mode=crop&bgcolor=black&format=jpg" alt=""/>
+//   </div><div className="w-full  p-6 sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 mb-4 bg-gray-500">
+//       <img className="rounded-lg" src="https://s3.ticketmx.com/uploads/images/ef7dcf538f9e4d9ea6e01bf3b813945857ab1d5b.jpeg?w=750&h=750&mode=crop&bgcolor=black&format=jpg" alt=""/>
+//   </div>
+//   <div className="w-full  p-6 sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 mb-4 bg-gray-500">
+//       <img className="rounded-lg" src="https://s3.ticketmx.com/uploads/images/ef7dcf538f9e4d9ea6e01bf3b813945857ab1d5b.jpeg?w=750&h=750&mode=crop&bgcolor=black&format=jpg" alt=""/>
+//   </div>
+//   <div className="w-full  p-6 sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 mb-4 bg-gray-500">
+//       <img className="rounded-lg" src="https://s3.ticketmx.com/uploads/images/ef7dcf538f9e4d9ea6e01bf3b813945857ab1d5b.jpeg?w=750&h=750&mode=crop&bgcolor=black&format=jpg" alt=""/>
+//   </div>
+//   <div className="w-full  p-6 sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 mb-4 bg-gray-500">
+//       <img className="rounded-lg" src="https://s3.ticketmx.com/uploads/images/ef7dcf538f9e4d9ea6e01bf3b813945857ab1d5b.jpeg?w=750&h=750&mode=crop&bgcolor=black&format=jpg" alt=""/>
+//   </div>
+//   <div className="w-full  p-6 sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 mb-4 bg-gray-500">
+//       <img className="rounded-lg" src="https://s3.ticketmx.com/uploads/images/ef7dcf538f9e4d9ea6e01bf3b813945857ab1d5b.jpeg?w=750&h=750&mode=crop&bgcolor=black&format=jpg" alt=""/>
+//   </div>
+//   <div className="w-full  p-6 sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 mb-4 bg-gray-500">
+//       <img className="rounded-lg" src="https://s3.ticketmx.com/uploads/images/ef7dcf538f9e4d9ea6e01bf3b813945857ab1d5b.jpeg?w=750&h=750&mode=crop&bgcolor=black&format=jpg" alt=""/>
+//   </div>
+//   </div>
         
     )
 }
